609ddb856024fb8db2553cfcc0093c708abdefa9
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-04-12 00:44:29 -0400

    removed broken component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 
 import NoMatch from "./pages/NoMatch";
@@ -11,13 +10,12 @@ import Saved from "./pages/Saved";
 import "./App.css";
 
 class App extends Component {
- 
+
   render() {
     return (
       <Router>
         <div>
           <NavBar />
-          <Header />
           <Wrapper>
             <Route exact path="/" component={Search} />
             <Route exact path="/search" component={Search} />
